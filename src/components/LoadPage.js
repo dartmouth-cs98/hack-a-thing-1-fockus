b76ee3db8c60da1c05b2c9ab61f95a24fc3a3941
Abubakar <abubakar.i.kasule.21@dartmouth.edu>
2020-09-22 07:06:26 -0400

    Boilerplate V.3.1 I forgot one thing. tbh should  not be a stndalone commit

--- src/components/LoadPage.js
+++ src/components/LoadPage.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 
 class LoadPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}; // nothing here yet
+
+    this.styles = {
+    };
+  }
+
   render() {
     return (
+      <div style={{}}>
 
+      </div>
     );
   }
 }
--- src/components/ReadPage.js
+++ src/components/ReadPage.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 class ReadPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}; // nothing here yet
+
+    this.styles = {
+    };
+  }
+  
   render() {
     return (
-        
+      
     )
   }
 }
