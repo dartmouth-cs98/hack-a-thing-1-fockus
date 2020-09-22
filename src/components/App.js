ce2346ab14ba9eac9cb1beb3bc8fbc01a36992a5
Abubakar <abubakar.i.kasule.21@dartmouth.edu>
2020-09-22 10:19:40 -0400

    we can read pdfs now. simple solution > complex solution.

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,12 @@ import ReadPage from './ReadPage';
 class App extends Component {
   render() {
     return (
-      <div style={{ widht: '100vw', height: '100vh', backgroundColor: 'rgba(234,134,232, 0.7)' }}>
-        <HashRouter>
-          <div>
-            <Route path="/" exact    component={ ReadPage } />
-            <Route path="/ReadPage"  component={ ReadPage } />
-          </div>
-        </HashRouter>
-      </div>
+      <HashRouter>
+        <div>
+          <Route path="/" exact    component={ ReadPage } />
+          <Route path="/ReadPage"  component={ ReadPage } />
+        </div>
+      </HashRouter>
     )
   }
 }
--- src/components/ReadPage.js
+++ src/components/ReadPage.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-// import { Document, Page } from 'react-pdf';
-import WebViewer from 'web-viewer'; 
-import doc from 'C:/Users/Abubakar/Desktop/CS98/hack-a-thing-1-fockus/src/components/webviewer-demo.pdf';
 
 class ReadPage extends Component {
   constructor(props) {
     super(props);
     this.state = {}; // nothing here yet
-
+    this.doc = import('C:/Users/Abubakar/Desktop/CS98/hack-a-thing-1-fockus/src/components/webviewer-demo.pdf');
+    console.log(this.doc);
     this.styles = {
       container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center',
         width: '100vw',
         height: '100vh',
         backgroundColor: 'rgba(12,124, 243, 0.6)',
@@ -18,16 +20,22 @@ class ReadPage extends Component {
   }
   
   render() {
-    return (
-      <div style={this.styles.container}>
-        <iframe src={doc} width='90%' height='90%'>
-
-          This browser does not support PDFs. Please download the PDF to view it: Download PDF
-
-        </iframe>
-      </div>
-      
-    )
+    if (this.doc == undefined){
+      return (
+        <div style={this.styles.container}>
+          Loading..**^^
+          <button onClick={() => {this.forceUpdate()}}>try again</button>
+        </div>
+      );
+    } else {
+      return (
+        <div style={this.styles.container}>
+          <iframe src={this.doc} width='90%' height='90%'>
+            This browser does not support PDFs. Please download the PDF to view it: Download PDF
+          </iframe>
+        </div>
+      );
+    }
   }
 }
 
